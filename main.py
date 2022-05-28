19ca54c383b7ef21d497ff7f28c045705d7336ec
Alieu <alieucsay099@gmail.com>
2022-05-28 09:40:54 +0100

    first code

---
+++ main.py
@@ -0,0 +1,73 @@
+# #  userName = "Ceesay"
+# # print(userName)
+# #  Number = 15
+# #  print(Number)
+# #  Number2 = 5
+# # result = Number + Number2
+# # print(result)
+# # Number = 15
+# # Number2 = 5
+# # result =int(Number / Number2)
+# # print(result)
+#
+# Number = 15
+# Number2 = 5
+# result =int(Number * Number2)
+# print(result)
+# c = 100
+# f = int((c*9/5)+32)
+# print(f)
+# question1
+# x = 10
+# x = 20
+# x = 15
+# print(x)
+# question2
+# x = 80
+# y = 8
+# z = x/y
+# print(z)
+# question3
+# question4
+# x = 25
+# y = 20
+# z = 3
+# f = 2
+# w = ((x-y)*z/f)
+# print(w)
+# question5
+# v = 20
+# x = v % 2
+# print(x)
+# question6
+# import math
+# f = 3**2
+# u = math.sqrt(f)
+# print(u)
+# import math
+# # a = 4
+# # vol = a**3
+# # area= 6*(a**2)
+# # print(f'this is the volume: {vol}')
+# # print(f'this is the area: {area}')
+# # q7
+# # Assignment
+# import math
+# a = 4
+# b = 21
+# c = 3
+# d = (b ** 2 - 4 * a * c)
+# sol1 = (-b-math.sqrt(d))/(2*a)
+# sol2 = (-b+math.sqrt(d))/(2*a)
+# print(sol1)
+# print(sol2)
+# # Use comments to show life to your work(critic)
+
+
+
+age = int(input('What is your age: '))
+age_remaining = 90 - age
+remaining_days = age_remaining * 365
+weeks_r = age_remaining * 52
+months_r = age_remaining * 12
+print(f'You have following {months_r} months \n {weeks_r} weeks\n  {remaining_days} days to live')
